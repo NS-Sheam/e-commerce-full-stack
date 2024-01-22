c89e72c09edd09c37498f00713a7bb81e5c61a58
NS-Sheam <123sheamfeni@gmail.com>
2024-01-22 14:10:07 +0600

    Refactor App component and add Redux and React Router support

--- e-commerce-client/src/main.tsx
+++ e-commerce-client/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/routes.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
