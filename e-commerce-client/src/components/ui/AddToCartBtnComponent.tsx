6ae836a5e12ed4218bce5f07e932566e3daff5c4
NS-Sheam <123sheamfeni@gmail.com>
2024-02-10 20:36:52 +0600

    Refactor AddToCartBtnComponent UI

--- e-commerce-client/src/components/ui/AddToCartBtnComponent.tsx
+++ e-commerce-client/src/components/ui/AddToCartBtnComponent.tsx
@@ -1,44 +1,72 @@
-import { Button, Tag } from "antd";
+import { Button, Col, Flex, Row, Tag } from "antd";
+import { FaShoppingCart } from "react-icons/fa";
 
 const AddToCartBtnComponent = () => {
   return (
-    <div>
-      <Tag style={{ padding: "0.3rem 0", fontWeight: "600", fontSize: "1.2rem" }}>
-        <span
-          style={{ fontSize: "1.5rem", borderRight: "1px solid #5f6c72", padding: "0 0.5rem" }}
-          className="cursor-pointer"
-        >
-          -
-        </span>{" "}
-        <span className="px-2">3</span>
-        <span
-          style={{ fontSize: "1.5rem", borderLeft: "1px solid #5f6c72", padding: "0 0.5rem" }}
-          className="cursor-pointer"
-        >
-          +
-        </span>
-      </Tag>
-
-      <Button
-        size="large"
-        style={{
-          width: "100%",
-          color: "#ffffff",
-          fontWeight: "bold",
-          border: "2px solid #fa8232",
-          backgroundColor: "#fa8232",
-          borderRadius: "0",
-        }}
+    <Row
+      gutter={[16, 16]}
+      justify={"center"}
+      align={"middle"}
+    >
+      <Col
+        span={8}
+        md={{ span: 4 }}
       >
-        Add to Cart
-      </Button>
-      <Button
-        size="large"
-        style={{ color: "#fa8232", fontWeight: "bold", border: "2px solid #fa8232" }}
+        <Tag
+          className="grid grid-cols-4 items-center justify-center text-center"
+          style={{ fontWeight: "600", fontSize: "1.2rem", width: "100%" }}
+        >
+          <span
+            style={{ fontSize: "1.5rem", borderRight: "1px solid #5f6c72", padding: "0.3rem 0" }}
+            className="cursor-pointer col-span-1"
+          >
+            -
+          </span>{" "}
+          <span className="col-span-2">33</span>
+          <span
+            style={{ fontSize: "1.5rem", borderLeft: "1px solid #5f6c72", padding: "0.3rem 0" }}
+            className="cursor-pointer col-span-1"
+          >
+            +
+          </span>
+        </Tag>
+      </Col>
+      <Col
+        span={16}
+        md={{ span: 12 }}
       >
-        Buy Now
-      </Button>
-    </div>
+        <Button
+          size="large"
+          style={{
+            width: "100%",
+            color: "#ffffff",
+            fontWeight: "bold",
+            border: "2px solid #fa8232",
+            backgroundColor: "#fa8232",
+            borderRadius: "0",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "0.5rem",
+          }}
+        >
+          Add to Cart <FaShoppingCart />
+        </Button>
+      </Col>
+      <Col
+        span={24}
+        md={{ span: 8 }}
+      >
+        <Flex justify="end">
+          <Button
+            size="large"
+            style={{ color: "#fa8232", fontWeight: "bold", border: "2px solid #fa8232", width: "100%" }}
+          >
+            Buy Now
+          </Button>
+        </Flex>
+      </Col>
+    </Row>
   );
 };
 
