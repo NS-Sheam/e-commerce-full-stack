3cf891d0ad0caa8419030acb6b33d344756c442e
NS-Sheam <123sheamfeni@gmail.com>
2024-03-31 22:09:15 +0600

    fixed magnifier

--- e-commerce-client/src/components/ui/ProductImageCarousel.tsx
+++ e-commerce-client/src/components/ui/ProductImageCarousel.tsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import { TProduct } from "../../types/product.types";
 import { Col, Flex, Row } from "antd";
-import { GlassMagnifier } from "react-image-magnifiers";
 import Magnifier from "react-magnifier";
 
 const ProductImageCarousel = ({ product }: { product: TProduct }) => {
+  const MagnifierComponent = Magnifier as any;
+
   const [sliderIndex, setSliderIndex] = useState(0);
   const arrowButtonStyle =
     "p-1 flex justify-center items-center rounded-full text-white text-xl absolute top-1/2 transform -translate-y-1/2";
@@ -23,35 +24,13 @@ const ProductImageCarousel = ({ product }: { product: TProduct }) => {
       >
         {/* FIXME: Zoom in second no image is not working. have to fix it. It is possible for image resulation */}
         <div className="flex justify-center items-center max-h-80 h-80 md:h-96 product-image-carousel-left ">
-          {/* <GlassMagnifier
-            style={{ width: "100%", height: "100%" }}
-            imageSrc={product?.images[sliderIndex]}
-            imageAlt={product.name}
-            largeImageSrc={product?.images[sliderIndex]}
-            magnifierSize="30%"
-            magnifierBorderSize={1}
-            magnifierBorderColor="rgba(0,0,0,0.5)"
-            square={true}
-            allowOverflow={true}
-            cursorStyle="none"
-            className="w-full h-full"
-          /> */}
-          <Magnifier
-            className="w-full h-full object-contain"
-            // height={300}
+          <MagnifierComponent
             src={product?.images[sliderIndex]}
-            // width={350}
             mgWidth={100}
             mgHeight={100}
             mgShape="square"
             mgShowOverflow={false}
-            mgBorderWidth={1}
           />
-          {/* <img
-            src={product?.images[sliderIndex]}
-            className="w-full h-full object-contain"
-            alt=""
-          /> */}
         </div>
       </Col>
       <Col
